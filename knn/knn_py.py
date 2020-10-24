f5e7ac9f6cf27f8ab9eb58013092ea30e4b08c91
kason-seu <zhangkaiseu@163.com>
2020-10-24 22:54:47 +0800

    knn

---
+++ knn/knn_py.py
@@ -0,0 +1,47 @@
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+
+raw_data_X = [[3.393533211, 2.331273381],
+              [3.110073483, 1.781539638],
+              [1.343808831, 3.368360954],
+              [3.582294042, 4.679179110],
+              [2.280362439, 2.866990263],
+              [7.423436942, 4.696522875],
+              [5.745051997, 3.533989803],
+              [9.172168622, 2.511101045],
+              [7.792783481, 3.424088941],
+              [7.939820817, 0.791637231]
+              ]
+raw_data_y = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1]
+
+x1 = np.array(raw_data_X)
+y = np.array(raw_data_y)
+
+plt.scatter(x1[y==0, 0], x1[y==0, 1], color='black')
+plt.scatter(x1[y==1,0],x1[y==1, 1],color='green')
+
+
+x = np.array([8.093607318, 3.365731514])
+
+plt.scatter(x[0],x[1], color='red', marker='+')
+plt.show()
+# 计算距离
+from math import sqrt
+distance = [ sqrt(np.sum((xx-x) ** 2)) for xx in x1]
+print(distance)
+
+distinceIndex = np.argsort(distance)
+print(distinceIndex)
+
+# 获取前三的排名
+K = 6
+votesTopK = [y[i] for i in distinceIndex[:K]]
+print(votesTopK)
+from collections import Counter
+
+votes = Counter(votesTopK)
+finalVote = votes.most_common(1)[0][0]
+print(finalVote)
